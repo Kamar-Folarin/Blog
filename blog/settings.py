a78d1c69fe787784d01366b411a0d1255b43f108
Kamar-Folarin <54280460+Kamar-Folarin@users.noreply.github.com>
2021-05-04 00:21:02 +0100

    updated allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['programmingblogapp.herokuapp.com']
+ALLOWED_HOSTS = ['aqueous-brook-18436.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
